39f05881dc67363b7068ed699ba33fcbeabbe7da
Da Vinci <105671369+SerGit-MondraHub@users.noreply.github.com>
2022-07-03 03:17:07 -0500

    succesful!!

--- main.js
+++ main.js
@@ -3,15 +3,24 @@
 //Objeto que contiene palabras para armar la frase...
 const palabras =
     {
-        sujetos:['Tu', 'tu pareja', 'tu familia', 'tu mascota', 'tu amig@'],
+        sujetos:['Tú ', 'tu pareja ', 'tu familia ', 'tu mascota ', 'tu amig@ '],
         verbos:['comprara', 'perdera', 'robara', 'quemara', 'vendera','cambiara'],
-        objetos:['un empleo', 'un carro', 'un diente', 'sexo', 'una casa', 'dinero', 'un(a) amante'],
+        objetos:[' empleo', ' un carro', ' un diente', ' sexo', ' una casa', ' dinero', ' un(a) amante'],
     }
 
+let frase=[]//ARRAY QUE CONTENDRA LAS 3 PALABRAS
 
 //Funcion randomizadora...
 function random (array)
-    {console.log(array[Math.floor(Math.random()*array.length)])}
+    {
+        let word;
+        word= (array[Math.floor(Math.random()*array.length)]);
+        /*if(word===Tú)
+            {frase.push(word)}
+        else
+            {frase.push(word)}*/
+        frase.push(word);
+    }
 
 //Funcion de iteracion...
 function iteration()   
@@ -21,6 +30,13 @@ function iteration()
                 //console.log(palabras[elem]);
                 random(palabras[elem]);
             }
+        if(frase[0]==='Tú ')//CONJUGACION DE VERBOS PARA "Tú"
+            {
+                frase.splice(2,0,'s');
+                console.log(frase.join(""));//MENSAJE FINAL CON "Tú"
+            }    
+        else    
+            {console.log(frase.join(""));}//MENSAJE FINAL SIN CONJUGACION
     }
 
 iteration();
