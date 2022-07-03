e4753b281ab3fb1db8575b3fd36234efe9f05add
Da Vinci <105671369+SerGit-MondraHub@users.noreply.github.com>
2022-07-02 23:17:51 -0500

    first commit

--- main1.js
+++ main1.js
@@ -1,17 +1,12 @@
 //BIENVENIDO A LAS GALLETAS DE LA FORTUNA DE SERGIO:
 
 //Objeto que contiene palabras para armar la frase...
-const frase =
+const palabras =
     {
         sujetos:['Tu', 'tu pareja', 'tu familia', 'tu mascota'],
         verbos:['conseguira', 'perdera', 'tendra','robara'],
         objetos:['un empleo', 'un carro', 'un diente', 'una casa'],
     }
 
-//Funcion randomizadora...
-function random (array)
-    {
-        console.log(array[Math.round(Math.random()*array.length)])
-    }
-
-random(frase.sujetos);
+function start()
+{console.log(this.array[Math.round(Math.random()*this.array.length)])}
