93fb0141fc80b9633011f44f92112443e613db59
Da Vinci <105671369+SerGit-MondraHub@users.noreply.github.com>
2022-07-02 20:23:21 -0500

    objeto frase

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('test2');
+//BIENVENIDO A LAS GALLETAS DE LA FORTUNA DE SERGIO:
+
+//Objeto que contiene palabras para armar la frase...
+const frase =
+    {
+        sujetos:[],
+        verbos:[],
+        objetos:[],
+    }
